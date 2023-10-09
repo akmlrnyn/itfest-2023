3082c9bd84dbf9c12fef1186155a350e9eca4192
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-10-09 09:19:53 +0700

    page web compe header, desc, and prizelist done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
     content: ['./src/**/*.{html,js}'],
     theme: {
         extend: {
+            fontFamily: {
+                matter: 'Matter',
+                ubuntu: 'Ubuntu'
+            },
+            colors: {
+                defaultBg: '#0D1727',
+                green: '#13AF6B',
+                162236: '#162236',
+            },
         },
     },
     plugins: [],
