e74c4bcef986ae5c3e51a62be0e26ebf4e98b300
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-10-10 11:31:18 +0700

    add flowbite navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{html,js}'],
+    content: ['./src/**/*.{html,js}', './node_modules/flowbite/**/*.js'],
     theme: {
         extend: {
             fontFamily: {
                 matter: 'Matter',
-                ubuntu: 'Ubuntu'
+                ubuntu: 'Ubuntu',
             },
             colors: {
                 defaultBg: '#0D1727',
                 green: '#13AF6B',
                 162236: '#162236',
+                blue: '#43ACFF',
             },
         },
     },
-    plugins: [],
+    plugins: [require('flowbite/plugin')],
 };
